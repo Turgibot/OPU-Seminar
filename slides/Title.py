27f1e930fd33def4aa0d33d8f4470868c4df0f96
Guy Tordjman <turgibot@gmail.com>
2021-09-18 22:00:00 +0300

    added itinerary scene

--- slides/Title.py
+++ slides/Title.py
@@ -8,7 +8,8 @@ class Header():
 
 class Title(SlideScene):
     def construct(self):
-        note = "This is a seminat about ..."
+        note = "Hello Everyone, my name is Guy Tordjman and today I'll present my seminar. This seminar reviews articles in 2 fields of neuromorphic engineering in robotics so I'm positive you'll find them very interesting.\n \
+            The First topic is neuromorphic stereo vision and the second one is neuromorphic Robot controller.\n In case you have question please feel free to stop me at any time."
         self.create_note(note)
         for x in Header().get():
             self.add(x)
@@ -16,10 +17,17 @@ class Title(SlideScene):
         self.add(cover)
         self.wait(0.2)
 
-class Abstract(SlideScene):
+class Itinerary(SlideScene):
     def construct(self):
+        note = "So the first topic that I would represent today is neuromorphic stereo vision. Inorder for me to so, I would like to start by briefly giving you all some background information about the subject of conventional stereo vision.\n \
+            I'll also present some slides with information about neuromorphic cameras and then we'll dive for a deeper explanation of the neuromorphic stereo vision model, decribed in the paper I reviewed"
+        self.create_note(note)
         for x in Header().get():
             self.add(x)
         cover = ImageMobject('../images/cover.png').scale(0.85)
+        name = Text("Guy Tordjman", font_size=12).shift(3.2*DOWN+6.3*LEFT)
         self.add(cover)
+        self.add(name)
+
+        # self.play(Transform(cover,name), run_time=1.5)
         self.wait(0.2)
