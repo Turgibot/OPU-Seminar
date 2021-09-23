e98002e3d28ac41698e234096ff93308d8895ee4
Guy Tordjman <turgibot@gmail.com>
2021-09-23 16:00:37 +0300

    Got to generative model

--- slides/StereoVision.py
+++ slides/StereoVision.py
@@ -1,3 +1,4 @@
+from numpy import number
 from manim_slide import *
 import cv2
 
@@ -89,7 +90,10 @@ class WhatIsEventCameraPart1(SlideScene):
 
 class WhatIsEventCameraPart2(SlideScene):
     def construct(self):
-        note = "Read the slide."
+        note = "Event camears were first commercialized in 2008 by Toby Delbruck from the university of Zurich\
+             under the name of DVS which stands for Dynamic Vision Sensor.\
+                 The sensor measures only motion in the scene. An event camera has smart pixels that are all\
+                     independent of each other - every time that a single pixel detects motion, that pixel trigers an event."
         self.create_note(note)
         for x in Header().get():
             self.add(x)
@@ -126,7 +130,12 @@ class WhatIsEventCameraPart2(SlideScene):
 
 class WhatIsEventCameraPart3(SlideScene):
     def construct(self):
-        note = "Read the slide."
+        note = "The animation on the bottom right of the screen from Davide Scaramuzza at the Robotics and perception group\
+             in the University of Zurich, compares the outputs of an event camera with a frame camera.\
+                    A rotating disk with a black circle is captured by the two camera types.\
+                        On top you can see the output of a standard camera which are intensity frames at a constant time interval,\
+                            while on the bottom you see the output of an event camera, which relates only to the moving black circlt and represented as a spiral of events in space and time \
+                                where the red and blue colors represent the polarity of the event."
         self.create_note(note)
         for x in Header().get():
             self.add(x)
@@ -168,7 +177,8 @@ class WhatIsEventCameraPart3(SlideScene):
 
 class WhatIsEventCameraPart4(SlideScene):
     def construct(self):
-        note = "Read the slide."
+        note = "When the disk stops rotating, there is no motion, hence the event camera does not transmit event\
+            which results in a drastic reduction of band width caompares to the standard camera that keepts putting out the same frame over and over again. "
         self.create_note(note)
         for x in Header().get():
             self.add(x)
@@ -209,7 +219,14 @@ class WhatIsEventCameraPart4(SlideScene):
 
 class WhatIsEventCameraPart5(SlideScene):
     def construct(self):
-        note = "Read the slide."
+        note = "When the disk rotates really fast, the standard camera undersamples and also blurs the images,\
+            while an ideal event camera outputs is a tighter spiral of events. \
+                so as we can see , Although Event camera have been around only for a few years, it posesses outstanding properties \
+            that have the potential of making strong impact in computer vision and robotics applications:\
+                1. a very low latency with a resolution of microsecond\
+                    2. no motion blur.\
+                        3. ultra low power consumption - an event camera on average consumes 1 milliWatt instead of 1 Watt in standard camera.\
+                            4. a very high dynamic range which is 8 orders of magnitude superior of standard cameras "
         self.create_note(note)
         for x in Header().get():
             self.add(x)
@@ -247,42 +264,108 @@ class WhatIsEventCameraPart5(SlideScene):
             
         cap.release()
 
-class ConStereoTitle(SlideScene):
+class Football(SlideScene):
     def construct(self):
-        note = "Read The Slides!!"
+        note = "Here we have a recording of a guy catching a football, taken with a davis camera which is an event camera that outputs both events and frames.\
+            In this case the the frame are being output at a rate of 6 Hz while a window of 10 milliseconds of events is shown. The frame data rate pick value mesured at th 30 kilo Hz which mean that\
+                So we are talking of faster analysis of the data which is a great benefit especially in real-time applications. \
+                    So I said the word event many times, I would like to finish this introduction by talking about the event generation process."
         self.create_note(note)
         for x in Header().get():
             self.add(x)
         name = Text("Guy Tordjman", font_size=11).shift(3.2*DOWN+6.3*LEFT)
-        self.add(name)
-        
-        title_conventional = Text("Classical Stereo Vision").shift(UP*3).scale(0.5) 
-        self.add(title_conventional)
+        source = Text("https://drive.google.com/file/d/1w7easbLhcHh-BoMFQq1RRt5RAjGkfk7u/view", font_size=11).shift(3.2*DOWN+3*RIGHT)
+        self.add(name, source)
+        title_camera = Text("What is an Event camera").shift(UP*3).scale(0.7)
+        self.add(title_camera)
 
-        bul0 = Text("Uses conventional stereo camera").scale(0.3).shift(LEFT*3.5 + UP)
-        bul1 = Text("Pixel/feature from one camera corresponds to a pixel/feature in the other camera.").scale(0.3).next_to(bul0, DOWN).align_to(bul0, LEFT)
-        bul2 = Text("This correspondece problem is solved using epipolar constraints.").scale(0.3).next_to(bul1, DOWN).align_to(bul1, LEFT)
-        bul3 = Text("The depth of in a certain pixel/feature is derived from its disparity.").scale(0.3).next_to(bul2, DOWN).align_to(bul2, LEFT)
 
-        self.play(Write(bul0), run_time = 0.75)
-        self.wait(2.5)
-        self.play(Write(bul1), run_time = 0.75)
-        self.wait(2.5)
-        self.play(Write(bul2), run_time = 0.75)
-        self.wait(2.5)
-        self.play(Write(bul3), run_time = 0.75)
-        self.wait(2.5)
+        bul0 = Text("First commercialized in 2008 by Toby Delbruck(UZH&ETH under the name of Dynamic Vision Sensor (DVS).)").scale(0.3).shift(LEFT*1.2 + 2*UP)
+        bul1 = Text("Novel sensor that measures the motion in the scene.").scale(0.3).next_to(bul0, DOWN).align_to(bul0, LEFT)
+        bul2 = Text("Ultra low power consumption (1 mW instead of 1W in standard ccd camera).").scale(0.3).next_to(bul1, DOWN).align_to(bul1, LEFT)
+        bul3 = Text("High dynamic range (140 dB instead of 80 dB in a standard ccd camera).").scale(0.3).next_to(bul2, DOWN).align_to(bul2, LEFT)
+        bul4 = Text("Low latency (~1 ms)").scale(0.3).next_to(bul3, DOWN).align_to(bul3, LEFT)
+        bul5 = Text("No motion blur").scale(0.3).next_to(bul4, DOWN).align_to(bul4, LEFT)
 
-class ClassicalExample(SlideScene):
+        bullets = Group(bul0,bul1,bul2,bul3,bul4,bul5)
+        self.add(bullets)
+        self.play(FadeOut(bullets, title_camera))
+        title_camera = Text("Output of an event camera").shift(UP*3).scale(0.7)
+        self.play(Write(title_camera, run_time=0.5))
+        self.wait(1)
+        #include video
+        davis_txt = Text("DAVIS Camera output. Frames @ 6Hz events window of 10ms").scale(0.3).shift(2.5*DOWN)
+        self.add(davis_txt)
+        cap = cv2.VideoCapture("../media/videos/football.mp4")
+        flag, frame = cap.read()
+        while not flag:
+            pass
+        while flag :
+            flag, frame = cap.read()
+            if flag:
+                frame_img = ImageMobject(frame).scale(1.1)
+                self.add(frame_img)
+                self.wait(0.042)
+                self.remove(frame_img)
+            
+        cap.release()
+
+class EventModelPart1(SlideScene):
     def construct(self):
-        note = "Read The Slides!!"
+        note = "As We have seen a traditional camera outputs frames at fixed time intervals,\
+            By contrast, event camera outputs asynchronous events at microseconds resolution. \
+                An event is generated each time a single pixel detects an intensity changes value.\
+                    each event consists of the following data: \
+                        1. The timestamp at the time of the trigering. \
+                            2. The x and y pixel coordinates in the sensor. \
+                                3. The event polarity (or sign) that get the value  of 1 or -1 for increase or decrease of brightness."
         self.create_note(note)
         for x in Header().get():
             self.add(x)
         name = Text("Guy Tordjman", font_size=11).shift(3.2*DOWN+6.3*LEFT)
-        self.add(name)
+        source = Text("http://rpg.ifi.uzh.ch/docs/EventVisionSurvey.pdf", font_size=11).shift(3.2*DOWN+3*RIGHT)
+        self.add(name, source)
+
+        title_camera = Text("Output of an event camera").shift(UP*3).scale(0.7)
+        self.add(title_camera)
+        title_event = Text("Generative Event Model").shift(UP*3).scale(0.7)
+        self.play(ReplacementTransform(title_camera, title_event))
         
-        title_conventional = Text("Classical Stereo Vision").shift(UP*3).scale(0.5) 
-        self.add(title_conventional)
+        #Clock
+        num = DecimalNumber(number=0, num_decimal_places=4)
+        unit = Tex("$sec$").next_to(num, RIGHT*0.5)
+        clock = VGroup(num, unit).shift(RIGHT*2+UP)
+        border = Rectangle(fill_opacity=1, fill_color=BLACK, stroke_color=BLUE)
+        border.round_corners().surround(clock)
+        self.add(border, clock)
 
+        #Graph for frames
+        x_start = 0
+        x_finish = 4
+        x_axis = NumberLine(x_range=[x_start, x_finish, 1], length=7, color=BLUE, include_numbers=True)
+        self.add(x_axis)
+        def add_arrow_to_axis(axis, t, interval=0.2, up=True):
+            if((t%interval)<1):
+                if up:
+                    pointer = Vector(UP)
+                else:
+                    pointer = Vector(DOWN)
+            
+                pointer.next_to(axis.n2p(t), UP)
+                self.add(pointer)
 
+        num.add_updater(lambda m, dt: m.set_value(m.get_value()+dt))
+        num.add_updater(lambda m, dt: add_arrow_to_axis(x_axis, m.get_value()) if (m.get_value()%0.2)<0.01667 and (m.get_value()%0.2)>=0.0166 else None)
+        self.wait(1)
+
+class GraphX(SlideScene):
+    def construct(self):
+
+        l0 = NumberLine(
+            x_range=[0, 10, 1],
+            length=10,
+            color=BLUE,
+            include_numbers=False,
+            label_direction=UP)
+        self.play(Write(l0))
+        time = ValueTracker(0)
